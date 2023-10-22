2804e90c3d817214695e554cb86e0a68aa88affd
eliasamaral <eliasseverianomaral@gmail.com>
2023-10-22 10:28:46 -0300

    fist commit in branch develop

--- src/graphql/modules/codigos/resolvers.js
+++ src/graphql/modules/codigos/resolvers.js
@@ -1,9 +1,10 @@
 import Codigo from "../../../models/Codigo";
 
 export default {
-
   Query: {
     codigos: async () => await Codigo.find(),
+
+
     codigo: async (_, { code }) => {
       try {
         const codigo = await Codigo.findOne({ code });
@@ -12,16 +13,38 @@ export default {
         }
         return codigo;
       } catch (error) {
-        console.error(error);
         throw new Error("Código não encontrado");
       }
     },
-    
+
+    codigoByDescription: async (parent, { description }, context, info) => {
+      try {
+        const a = await Codigo.findOne({ description });
+        if (!a) {
+          throw new Error("Descrição não encontrada");
+        }
+        return a;
+      } catch (error) {
+        throw new Error(error);
+      }
+    },
+
+    codigoByType: async (parent, { type }, context, info) => {
+      try {
+        const a = await Codigo.find({ type });
+        if (!a) {
+          throw new Error("Tipo não encontrada");
+        }
+        return a;
+      } catch (error) {
+        throw new Error(error);
+      }
+    },
   },
   Mutation: {
     createCodigo: (_, { data }) => Codigo.create(data),
-    updateCodigo: async (_, { id, data }) =>
-      await Codigo.findByIdAndUpdate(id, data, { new: true }),
-    deleteCodigo: async (_, { id }) => !!(await Codigo.findByIdAndRemove(id)),
+    updateCodigo: async (_, { _id, data }) =>
+      await Codigo.findByIdAndUpdate(_id, data, { new: true }),
+    deleteCodigo: async (_, { _id }) => !!(await Codigo.findByIdAndRemove(_id)),
   },
 };
