87b6895a515940bb074165e067e824b254f52d00
eliasamaral <eliasseverianomaral@gmail.com>
2023-10-22 12:18:44 -0300

    mudança na estrutura de dados de RDOs

--- src/models/RDO.js
+++ src/models/RDO.js
@@ -5,59 +5,86 @@ const mongoose = require("mongoose");
 const Schema = new mongoose.Schema({
   projeto: {
     type: Number,
+    required: true
   },
 
   diagrama: {
     type: Number,
+    required: true
   },
 
   local: {
     type: String,
+    required: true
   },
   encarregado: {
     type: String,
+    required: true
   },
-  climaManha: {
-    type: String,
+  clima: {
+    manha: {
+      type: String,
+      required: true
+    },
+
+    tarde: {
+      type: String,
+      required: true
+    },
   },
-  climaTarde: {
-    type: String,
+
+  maoDeObra: {
+    encarregado: {
+      type: Number,
+      required: true
+    },
+    motorista: {
+      type: Number,
+      required: true
+    },
+    eletricista: {
+      type: Number,
+      required: true
+    },
+    auxiliar: {
+      type: Number,
+      required: true
+    },
   },
-  encarregadoQuantidade: {
-    type: Number,
-  },
-  motoristaQuantidade: {
-    type: Number,
-  },
-  eletricistaQuantidade: {
-    type: Number,
-  },
-  auxiliarQuantidade: {
-    type: Number,
-  },
+
   observacoes: {
     type: String,
+    required: true
   },
   dataDaProducao: {
     type: String,
+    required: true
   },
   createdAt: {
     type: Date,
+    required: true,
     default: Date.now,
   },
   servicos: [
     {
       codigo: {
         type: Number,
+        required: true
       },
       descricao: {
         type: String,
+        required: true
       },
       quantidade: {
         type: Number,
+        required: true
       },
     },
   ],
+  isFinal: {
+    type: Boolean,
+    require: true,
+  },
 });
 
 export default mongoose.model("RDO", Schema);
