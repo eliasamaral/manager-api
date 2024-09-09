618a802723695ef5cd3a5f3b5d7fab5bb2e828a7
Elias Severiano Amaral <eliasseverianoamaral@gmail.com>
2024-09-09 15:43:50 -0300

    modificação para novo forms mte

--- src/models/RDO.js
+++ src/models/RDO.js
@@ -1,104 +1,77 @@
 const mongoose = require("mongoose");
 
-//lebrar de tornar required: true
-
-const Schema = new mongoose.Schema({
-  projeto: {
-    type: Number,
-    required: true,
-  },
-
-  diagrama: {
-    type: Number,
-    required: true,
-  },
-
-  local: {
-    type: String,
-    required: true,
-  },
-  encarregado: {
-    type: String,
-    required: true,
-  },
-  clima: {
-    manha: {
-      type: String,
-      required: true,
-    },
-
-    tarde: {
-      type: String,
-      required: true,
-    },
-  },
-
-  maoDeObra: {
-    encarregado: {
-      type: Number,
-      required: true,
-    },
-    motorista: {
-      type: Number,
-      required: true,
-    },
-    eletricista: {
-      type: Number,
-      required: true,
-    },
-    auxiliar: {
-      type: Number,
-      required: true,
-    },
-  },
-
-  observacoes: {
-    type: String,
-    required: true,
-  },
-  dataDaProducao: {
-    type: String,
-    required: true,
-  },
-  createdAt: {
-    type: Date,
-    required: true,
-    default: Date.now,
-  },
-
-  fichaTrafo: {
-    estf: { type: String },
-    estfsucata: { type: String },
-    nSerie: { type: String },
-    nSucataSerie: { type: String },
-    NA: { type: String },
-    NB: { type: String },
-    NC: { type: String },
-    AB: { type: String },
-    AC: { type: String },
-    BC: { type: String },
-  },
-
-  servicos: [
-    {
-      codigo: {
-        type: Number,
-        required: true,
-      },
-      descricao: {
-        type: String,
-        required: true,
-      },
-      quantidade: {
-        type: Number,
-        required: true,
-      },
-    },
-  ],
-  isFinal: {
-    type: Boolean,
-    require: true,
-  },
+const ClimaSchema = new mongoose.Schema({
+	manha: {
+		type: String,
+		required: true,
+	},
+	tarde: {
+		type: String,
+		required: true,
+	},
 });
+
+const AtividadeSchema = new mongoose.Schema({
+	atividade: {
+		type: String,
+		required: true,
+	},
+	duracao: {
+		type: String,
+		required: true,
+	},
+});
+
+const MaoDeObraSchema = new mongoose.Schema({
+	nome: {
+		type: String,
+		required: true,
+	},
+	funcao: {
+		type: String,
+		required: true,
+	},
+	inicio: {
+		type: String,
+		required: true,
+	},
+	fim: {
+		type: String,
+		required: true,
+	},
+});
+
+const Schema = new mongoose.Schema(
+	{
+		projeto: {
+			type: String,
+			required: true,
+		},
+		local: {
+			type: String,
+			required: true,
+		},
+		encarregado: {
+			type: String,
+			required: true,
+		},
+		observacoes: {
+			type: String,
+		},
+		clima: {
+			type: ClimaSchema,
+			required: true,
+		},
+		dataDaProducao: {
+			type: String,
+			required: true,
+		},
+		atividades: [AtividadeSchema],
+		maoDeObra: [MaoDeObraSchema],
+	},
+	{
+		timestamps: true,
+	},
+);
 
 export default mongoose.model("RDO", Schema);
